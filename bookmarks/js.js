9ad4501416ed35a5497b1cedbf16981ab79a553c
weblongzhao <longzhao135@gmail.com>
2018-07-26 17:44:41 +0800

    js first finish

--- bookmarks/js.js
+++ bookmarks/js.js
@@ -26,19 +26,35 @@ for(var index =0;index<3;index++) {
         kbdBox.appendChild(div) ;
         div.appendChild(kbd) ;
         kbd.innerText=keys[index][i] ;
+        kbd.setAttribute("data-target",keys[index][i]);
         img.src='http://www.'+hash[keys[index][i]]+'/favicon.ico';
         kbd.appendChild(img) ;
     }
 }
 
 //添加键盘事件
-document.onkeypress = function(xxx){
-    console.log(xxx.key);
+document.onkeydown = function(xxx){
+    var key = xxx.key;
+    var ele =document.querySelector(`kbd[data-target=${key}]`);
+    // console.log(document.querySelector("kbd[data-target='+xxx.key+']"));
+    ele.className='active';
+    console.log(hash[xxx.key]);
+    window.location=`http://www.${hash[xxx.key]}`
 
 
 };
+//抬起键盘事件
+document.onkeyup = function (xxx){
+    var key = xxx.key;
+    var ele =document.querySelector(`kbd[data-target=${key}]`);
+    // console.log(document.querySelector("kbd[data-target='+xxx.key+']"));
+    ele.className=''
+};
 
-
+// 添加编辑功能
+kbdBox.onclick = function(xx){
+    console.log(xx.target);
+};
 
 
 
