9b557f1a2649b729f01b8b07942ff973840b378d
weblongzhao <longzhao135@gmail.com>
2018-07-27 13:17:13 +0800

    js finish v2

--- bookmarks/js.js
+++ bookmarks/js.js
@@ -13,7 +13,6 @@ var hash = {
     'y':'qq.com',
     'u':'qq.com'
 } ;
-
 // 生成 dom元素
 for(var index =0;index<3;index++) {
     var div = document.createElement('div') ;
@@ -21,14 +20,22 @@ for(var index =0;index<3;index++) {
     for(var i=0;i<keys[index].length;i++){
         var kbd = document.createElement('kbd') ;
         var img = document.createElement('img') ;
-        //域名
+        var edit = document.createElement('span');
+        edit.innerText = 'E';
+        edit.className="edit";
+        //获取域名
+
         console.log(hash[keys[index][i]]);
         kbdBox.appendChild(div) ;
         div.appendChild(kbd) ;
         kbd.innerText=keys[index][i] ;
         kbd.setAttribute("data-target",keys[index][i]);
+        if(localStorage.getItem("href")!=null){
+            load() ;
+        }
         img.src='http://www.'+hash[keys[index][i]]+'/favicon.ico';
         kbd.appendChild(img) ;
+        kbd.appendChild(edit);
     }
 }
 
@@ -38,10 +45,11 @@ document.onkeydown = function(xxx){
     var ele =document.querySelector(`kbd[data-target=${key}]`);
     // console.log(document.querySelector("kbd[data-target='+xxx.key+']"));
     ele.className='active';
+    if(localStorage.getItem("href")!=null){
+        load() ;
+    };
     console.log(hash[xxx.key]);
     window.location=`http://www.${hash[xxx.key]}`
-
-
 };
 //抬起键盘事件
 document.onkeyup = function (xxx){
@@ -53,8 +61,22 @@ document.onkeyup = function (xxx){
 
 // 添加编辑功能
 kbdBox.onclick = function(xx){
-    console.log(xx.target);
+    console.log(xx.path);
+    var kbd = xx.path[1].dataset.target;
+    if(xx.target.className === "edit") {
+        var href=prompt('请输入新网址');
+        xx.path[1].firstElementChild.src='http://www.'+href+'/favicon.ico';
+        save(kbd,href);
+        load();
+    }
 };
-
-
-
+//存储localstorage
+function save(kbd,href){
+    hash[kbd]=href;
+    localStorage.setItem("href", JSON.stringify(hash));
+}
+//加载href
+function load(){
+    console.log(JSON.parse(localStorage.getItem("href")));
+    hash=JSON.parse(localStorage.getItem("href"));
+}
