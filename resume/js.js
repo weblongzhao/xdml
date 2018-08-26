c1e75ceb891c8dc2d183e059d6661cc7e888522d
weblongzhao <longzhao135@gmail.com>
2018-08-26 12:56:59 +0800

    导航栏点击滑动

--- resume/js.js
+++ resume/js.js
@@ -4,4 +4,19 @@ window.onscroll=function(xx){
     }else {
         document.querySelector('.topNavBar').classList.remove('sticky')
     }
-}
+};
+
+// 点击导航滑动到指定位置
+
+let lias = document.querySelectorAll(".topNavBar  nav li>a");
+var num=0;
+lias.forEach(function(val,key){
+    val.onclick=function (x) {
+        let targetName=x.currentTarget.getAttribute('href');
+        // document.querySelector('["data-'+targetName+'"]');
+        console.log(document.querySelector('[data-target="#skills"]').offsetTop);
+        window.scrollTo(0,document.querySelector("[data-target='"+targetName+"']").offsetTop-60);
+    };
+
+});
+
